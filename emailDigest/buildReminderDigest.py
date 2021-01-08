5897eac509f4650e57e64dcc79915c2be86a3584
Matthew M <514050+mjmcfalls@users.noreply.github.com>
2021-01-08 18:23:07 -0500

    Feaure: Build out dev logic, py script to digest reminders

---
+++ emailDigest/buildReminderDigest.py
@@ -0,0 +1,74 @@
+import logging
+import requests
+import re
+from datetime import datetime, timedelta
+import pytz
+from pytz import timezone
+import json
+
+envfile = '.env'
+contactsStub = "contacts"
+reminderStub = "reminders"
+followups = []
+events = []
+
+def getRemoteData(url, headers, payload):
+    tempData = []
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    # logging.info("Response Status: {}".format(response.status_code))
+
+    tempData.extend(response.json()['data'])
+    nextUrl = response.json()['links']['next']
+
+    while nextUrl:
+        response = requests.request("GET", nextUrl, headers=headers, data=payload)
+        logging.info("Response Status: {}".format(response.status_code))
+        tempData.extend(response.json()['data'])
+        nextUrl = response.json()['links']['next']
+    
+    return tempData
+
+def main():
+    contacts = []
+    reminders = []
+
+    with open(envfile, "r") as ifile:
+        envDict = json.load(ifile)
+
+    cDatetime = datetime.now()
+    
+    contactsUri = "{}{}".format(envDict['url'],contactsStub)
+    remindersUri = "{}{}".format(envDict['url'],reminderStub)
+    payload={}
+    headers = {'Authorization': "Bearer {}".format(envDict['token'])}
+    
+    contacts = getRemoteData(url=contactsUri, headers=headers, payload=payload)
+    reminders = getRemoteData(url=remindersUri, headers=headers, payload=payload)
+
+    for contact in contacts: 
+        if 'stay_in_touch_trigger_date' in contact:
+
+            if contact['stay_in_touch_trigger_date']:
+                targetDate = datetime.strptime(contact['stay_in_touch_trigger_date'], '%Y-%m-%dT%H:%M:%SZ')
+                if targetDate.date() - cDatetime.date() <= timedelta(days=envDict['contactDays']):
+                    followups.append(contact)
+                    logging.info("{} {} - Next Contact: {}".format(contact["first_name"],contact["last_name"],contact['stay_in_touch_trigger_date'].split('T')[0]))
+
+    # for item in followups:
+        # logging.info("{} {} - Next Contact: {}".format(item["first_name"],item["last_name"],item['stay_in_touch_trigger_date'].split('T')[0]))
+    for item in reminders:
+        initial_date = datetime.strptime(item['initial_date'], '%Y-%m-%dT%H:%M:%SZ').replace(year=cDatetime.year)
+        # logging.info("{}".format(initial_date.date() - cDatetime.date()))
+        days = initial_date.date() - cDatetime.date()
+        if days <= timedelta(days=envDict['reminderDays']) and days > timedelta(days=0) :
+            # logging.info("{}".format(days.days))
+            logging.info("{} {} - {}: {} days on {}-{}".format(item['contact']["first_name"],item['contact']["last_name"],item['title'],days.days,initial_date.month,initial_date.day))
+        # else:
+            # logging.info("{} {} - {}: {} days on {}-{}".format(item['contact']["first_name"],item['contact']["last_name"],item['title'],days.days,initial_date.month,initial_date.day))
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,handlers=[logging.StreamHandler()], format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",)
+    main()
